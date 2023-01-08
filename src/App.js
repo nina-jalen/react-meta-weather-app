3c0cc204c65570546d0fee87ccda9cdd8ca7447b
nina-jalen <99284449+nina-jalen@users.noreply.github.com>
2023-01-08 12:02:22 +0100

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-	return <div className="App">Meta Weather App</div>;
+	return (
+		<div className="App">
+			<div className="container">Meta Weather App</div>
+		</div>
+	);
 }
