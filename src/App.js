4bb2005653231b117072d35607234262926a2b8d
nina-jalen <99284449+nina-jalen@users.noreply.github.com>
2023-01-08 16:16:12 +0100

    Adding Google Fonts and Font Awesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+// font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationArrow } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
 	return (
 		<div className="App">
 			<div className="container">React Meta Weather App</div>
+			<FontAwesomeIcon icon={faLocationArrow} />
+			<FontAwesomeIcon icon={faMagnifyingGlass} />
 		</div>
 	);
 }
