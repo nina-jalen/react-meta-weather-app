2ee30ee0d03ff5cc63411b7269f9bee2893082e1
nina-jalen <99284449+nina-jalen@users.noreply.github.com>
2023-01-26 19:21:20 +0100

    Editing site layout

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,11 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+// font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// font awesome icons
+import { faDroplet, faWind } from "@fortawesome/free-solid-svg-icons";
+
 import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
@@ -12,6 +17,8 @@ export default function WeatherInfo(props) {
 			<span className="country-name">{props.data.country}</span>
 			{/* date */}
 			<FormattedDate date={props.data.date} />
+			{/* weather descripiton */}
+			<div className="weather-description">{props.data.description}</div>
 			{/* current weather */}
 			<div className="row justify-content-start m-0 p-0 row-parent">
 				{/* icon */}
@@ -22,23 +29,38 @@ export default function WeatherInfo(props) {
 						size={70}
 					/>
 				</div>
+				{/* current temperature & unit conversion */}
 				<div className="col-auto ms-3 p-0">
-					{/* current temperature & unit conversion */}
 					<WeatherTemperature celsius={props.data.temperature} />
-					{/* weather data */}
 				</div>
+				{/* weather data */}
 				<div className="col-auto ms-4 p-0 d-none d-md-block">
+					<div>
+						<FontAwesomeIcon
+							icon={faWind}
+							color="white"
+							opacity={0.8}
+							className="dataIcon"
+						/>
+					</div>
 					<div className="data-value">
-						{Math.round(props.data.wind * 0.001 * 3600)}&nbsp;km/h
+						{Math.round(props.data.wind * 0.001 * 3600)}km/h
 					</div>
 					<div className="data-description">Wind</div>
 				</div>
 				<div className="col-auto ms-3 p-0 d-none d-md-block">
+					<div>
+						<FontAwesomeIcon
+							icon={faDroplet}
+							color="white"
+							opacity={0.8}
+							className="dataIcon"
+						/>
+					</div>
 					<div className="data-value">{props.data.humidity}%</div>
-					<div className="data-description mb-1">Humidity</div>
+					<div className="data-description">Humidity</div>
 				</div>
 			</div>
-			<div className="weather-description">{props.data.description}</div>
 		</div>
 	);
 }
