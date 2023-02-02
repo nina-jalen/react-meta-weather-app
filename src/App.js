67d925510046a392b1de185cf6c54ba1f8607af3
nina-jalen <99284449+nina-jalen@users.noreply.github.com>
2023-02-02 17:43:35 +0100

    Aligning WeatherInfo elements

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
 	return (
 		<div className="App">
-			<div className="container">
+			<div className="Container">
 				<Weather defaultCity="Innsbruck" />
 			</div>
 		</div>
