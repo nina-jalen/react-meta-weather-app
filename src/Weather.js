bf5f9b3928a25a04a73b6d9d411e1231e6a93be2
nina-jalen <99284449+nina-jalen@users.noreply.github.com>
2023-08-11 18:35:27 +0200

    Testing SVG icon responsiveness

--- src/Weather.js
+++ src/Weather.js
@@ -68,6 +68,7 @@ export default function Weather(props) {
 								role="img"
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 512 512"
+								className="buttonIcon"
 							>
 								<path
 									fill="currentColor"
